ab4e4844bc7fb00c9a4afc5f431db2856bd3327f
David Shere <david.e.shere@gmail.com>
2013-04-10 19:30:33 -0700

    Added a few more instructions to the Emulatore core.

--- Chip8/Emulator.hs
+++ Chip8/Emulator.hs
@@ -40,18 +40,53 @@ execute' m (LDReg   vx vy)  = do
     (Mem8 y) <- load m (Register vy)
     store m (Register vx) (Mem8 y)
 execute' m (OR      vx vy)  = do
-    (Mem8 x) <- load m (Register vx)
-    result   <- loadf (x .|.) m (Register vy)
-    store m (Register vx) result
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register vx) (toMem8 $ x .|. y)
 execute' m (AND     vx vy)  = do
-    (Mem8 x) <- load m (Register vx)
-    result   <- loadf (x .&.) m (Register vy)
-    store m (Register vx) result
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register vx) (toMem8 $ x .&. y)
 execute' m (XOR     vx vy)  = do
-    (Mem8 x) <- load m (Register vx)
-    result   <- loadf (x `xor`) m (Register vy)
-    store m (Register vx) result
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register vx) (toMem8 $ x `xor` y)
+execute' m (ADDReg  vx vy)  = do
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register VF) (toMem8 $ if x + y > 255 then 1 else 0)
+    store m (Register vx) (toMem8 $ x + y)
+execute' m (SUB vx vy)      = do
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register VF) (toMem8 $ if x > y then 1 else 0)
+    store m (Register vx) (toMem8 $ x - y)
+execute' m (SHR vx)         = do
+    x <- loadInt m (Register vx)
+    store m (Register VF) (toMem8 $ x .&. 0x01)
+    store m (Register vx) (toMem8 $ x `shiftR` 1)
+execute' m (SUBN vx vy)     = do
+    x <- loadInt m (Register vx)
+    y <- loadInt m (Register vy)
+    store m (Register VF) (toMem8 $ if x < y then 1 else 0)
+    store m (Register vx) (toMem8 $ y - x)
+execute' m (SHL vx)         = do
+    x <- loadInt m (Register vx)
+    store m (Register VF) (toMem8 $ (x .&. 0x80) `shiftR` 7)
+    store m (Register vx) (toMem8 $ x `shiftL` 1)
+execute' m (SNEAddr vx vr)  = return () -- todo
 
 loadf f m r = do
     (Mem8 x) <- load m r
     return (Mem8 $ f x)
+
+loadInt :: Memory -> Address -> IO Int
+loadInt m (Register I) = do
+    (Mem16 x) <- load m (Register I)
+    return . fromIntegral $ x
+loadInt m addr = do
+    (Mem8  x) <- load m addr
+    return . fromIntegral $ x
+
+toMem8  x = Mem8  $ fromIntegral x
+toMem16 x = Mem16 $ fromIntegral x
--- Chip8/Memory.hs
+++ Chip8/Memory.hs
@@ -23,10 +23,10 @@ import Data.Array.IO
 import Data.IORef
 
 data Register
-    = V0  | V1  | V2  | V3
-    | V4  | V5  | V6  | V7
-    | V8  | V9  | V10 | V11
-    | V12 | V13 | V14 | V15
+    = V0 | V1 | V2 | V3
+    | V4 | V5 | V6 | V7
+    | V8 | V9 | VA | VB
+    | VC | VD | VE | VF
     | I
     deriving (Bounded, Enum, Eq, Show)
 
